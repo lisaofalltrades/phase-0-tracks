324740f22051483f1921a2a05e8c497ea66e68f6
echo 'hello world <lisathuanguyen@gmail.com>
2016-09-19 08:45:57 -0700

    Adding new hire questionaires to VAMPIRES.RB

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+name = gets.chomp
+puts "What year were you born?"
+birthday = gets.chomp.to_i
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (Yes/No)"
+adoption = gets.chomp
+puts "Would you like to enroll in the company’s health insurance? (Yes/No)"
+est_age = gets.chomp
