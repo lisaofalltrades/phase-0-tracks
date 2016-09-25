4d2b7dbf0fd8b4b5df5fb84bb7f609eb8abf4d14
echo 'hello world <lisathuanguyen@gmail.com>
2016-09-25 09:15:29 -0700

    Saving answers to final 4 questions

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -35,15 +35,19 @@
 #"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+#"The case of the disappearing first letter".replace("he case of the disappearing first letter")
+#"The case of the disappearing first letter"[1, 40]
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+#"Elementary,    my   dear        Watson!".squeeze
+#{}"Elementary,    my   dear        Watson!".replace("Elementary, my dear Watson!")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+# "z".ord
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+# The significance is that 122 is the integer ordinal of "z". An ordinal is the position of an
+# item in a list, and they correspond to an ASCII value list.
+#http://www.theasciicode.com.ar/ascii-printable-characters/lowercase-letter-z-minuscule-ascii-code-122.html
 
-# "How many times does the letter 'a' appear in this string?".<???>
+# "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
