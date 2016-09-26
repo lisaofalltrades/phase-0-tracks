1d6712f1f75e025cc74ec554ea43dfebab8f7ab2
echo 'hello world <lisathuanguyen@gmail.com>
2016-09-26 08:44:56 -0700

    saving pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,6 @@
+#Pseudocode
+#Split name at spaces, reverse items in array, and rejoin
+#Split name into characters
+#map each character to next vowel or consonant using get_next method
+# - if character is not a letter, return the character without changing it
+# - check whether vowel or consonant and map to next vowel or consonant accordingly
