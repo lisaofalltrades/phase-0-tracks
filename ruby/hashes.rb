555557557a1f8ed3036ad14360c1e15345d44180
echo 'hello world <lisathuanguyen@gmail.com>
2016-09-25 20:46:08 -0700

    Saving Pseudocode for HASHES.RB

---
+++ ruby/hashes.rb
@@ -0,0 +1,17 @@
+=begin
+5.2 Release 2 : Use a Hash in a Program
+Pseudocode
+-Welcome user
+-Ask the user for the following
+	-clients name
+	-clients age
+	-number of childen?
+	-decor theme
+	-decor pet peeve
+	-Do you have a pool? (True/False)
+-store info in a hash after each entry
+-Summarize all data for user
+-Thank the user and exit
+-If the entry is not "none", allow the user to update the value for the key they input
+-Print the updated version of the hash to the screen
+=end
