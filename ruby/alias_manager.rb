3e0200dc834cdd1181b9405551897bcfa18b55e1
echo 'hello world <lisathuanguyen@gmail.com>
2016-09-26 08:54:18 -0700

    Saving methods and interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -12,3 +12,40 @@ end
 def is_letter(letter)
 	letter =~ /[a-zA-Z]/
 end
+
+def get_next(letter)
+	letter == 'z' ? scan_letter = "a" : scan_letter = letter.next
+	if !is_letter(letter)
+		scan_letter = letter
+	elsif is_vowel(letter)
+		while !is_vowel(scan_letter)
+			scan_letter.next!
+		end
+	elsif !is_vowel(letter)
+		while its_vowel(scan_letter)
+			scan_letter.next!
+		end
+	end
+	scan_letter
+end
+
+def alias_manager(name)
+	agent_alias = name.split(' ').reverse.join(' ').split(' ')
+	agent_alias.map! {|letter|} get_next(letter)}
+	agent_alias.join(' ')
+end
+
+input = nil
+aliases = {}
+
+loop do 
+	puts "Please enter your full name:"
+	input = gets.chomp
+	break if input == "quit"
+	aliases.merge!(input.to_sym => alias_manager(input))	
+end
+
+aliases.each { |key, value| puts "#{key} is also known as #{value}."}
+
+	
+		
