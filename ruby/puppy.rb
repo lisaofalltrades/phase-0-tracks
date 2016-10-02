7513b28d3958b596c5dc3fc6deaf4b7e8ff7a944
echo 'hello world <lisathuanguyen@gmail.com>
2016-10-02 10:35:14 -0700

    Saving PUPPY.RB

---
+++ ruby/puppy.rb
@@ -0,0 +1,25 @@
+# SPECIES ------------------------
+# Adorableus Canine
+
+# CHARACTERISTICS ----------------
+# Fur Length: short
+# Potty trained: yes
+# Friendly: yes
+# Playful: yes
+# Name: varies
+# Color: varies
+# Paws: 4
+# Tail: 1
+
+# BEHAVIOR -----------------------
+# Cuddly
+# Playful
+# Sleepy
+# Loyal
+
+class Puppy
+end
+
+french_bulldog = Puppy.new
+german_shepard = Puppy.new
+shiba = Puppy.new
