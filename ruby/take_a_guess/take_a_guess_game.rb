905879f31354ada755f904034574983ee838f6f3
echo 'hello world <lisathuanguyen@gmail.com>
2016-10-03 07:45:40 -0700

    Saving guess_amount and guess_char methods

--- ruby/take_a_guess/take_a_guess_game.rb
+++ ruby/take_a_guess/take_a_guess_game.rb
@@ -19,4 +19,31 @@ class Take_a_guess
 		@hidden_phrase = @hidden_phrase.join
 		return @hidden_phrase
 	end
+	def guess_amount(obfuscated_phrase)
+		obfuscated_phrase.split('').each do |character|
+			if character == '_'
+				@total_guesses_available += 1
+			end
+		end
+		return @total_guesses_available
+	end
+	def guess_char(character, stored_phrase, obfuscated_phrase)
+		@guess_array.each do |char|
+			if char == character
+				@guess_count -= 1
+				break
+			end
+		end
+		@guess_array << character
+		index = 0
+		stored_phrase.split('').each do |letter|
+			if character == letter 
+				obfuscated_phrase[index] = character
+			end
+			index += 1
+		end
+		@guess_count += 1
+		@hidden_phrase = obfuscated_phrase
+		return @hidden_phrase
+	end
 end
