3d3d697ca35878bf8d4cd8a69b285d87f449f7b8
echo 'hello world <lisathuanguyen@gmail.com>
2016-10-02 22:08:27 -0700

    Saving birthdate and reindeer favorites

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,4 +14,12 @@ class Santa
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
 	end
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete(reindeer)
+		@reindeer_ranking.push(reindeer)
+	end
 end
