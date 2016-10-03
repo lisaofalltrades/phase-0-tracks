4ef6fbd4384ecb8ff4ded73fc903379d8ee8e684
echo 'hello world <lisathuanguyen@gmail.com>
2016-10-02 22:10:19 -0700

    Saving initialization code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -22,4 +22,37 @@ class Santa
 		@reindeer_ranking.delete(reindeer)
 		@reindeer_ranking.push(reindeer)
 	end
+	# def gender=(gender)
+	# 	@gender = gender
+	# end
+
+	# def age
+	# 	@age
+	# end
+
+	# def ethnicity
+	# 	@ethnicity
+	# end
 end
+
+# santa = Santa.new
+
+# santa.speak
+# santa.eat_milk_and_cookies("chocolate chip")
+
+# arr_of_santa_objects = []
+# genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# ethnicities = ["black", "Latino", "white", "Japanese", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# index = 0
+# genders.each do |gender|
+# 	arr_of_santa_objects << Santa.new(gender, ethnicities[index])
+# 	index += 1
+# end
+
+# arr_of_santa_objects[0].celebrate_birthday
+# arr_of_santa_objects[0].get_mad_at("Rudolph")
+# arr_of_santa_objects[0].gender = "gender neutral"
+# p arr_of_santa_objects[0].age
+# p arr_of_santa_objects[0].ethnicity
+
+# p arr_of_santa_objects[0]
