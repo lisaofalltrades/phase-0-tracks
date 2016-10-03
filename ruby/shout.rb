5218b29323aa5bd242dcf795841d01f4f7d480b1
echo 'hello world <lisathuanguyen@gmail.com>
2016-10-02 22:33:32 -0700

    Added 2 new classes and driver code for a football crowd and football cheerleader

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -10,3 +10,19 @@ end
 
 # p Shout.yell_angrily("AHHHH, I HATE EVERYONE")
 # p Shout.yell_happily("AHHHH, I LOVE EVERYONE")
+
+class Football_crowd
+	include Shout
+end
+
+class Football_cheerleader
+	include Shout
+end
+
+Football_crowd = Football_crowd.new
+p football_crowd.yell_angrily("THAT'S A FOUL!!!")
+p football_crowd.yell_happily("TOUCHDOWN!!!")
+
+Football_cheerleader = Football_cheerleader.new
+p football_cheerleader.yell_angrily("M-I-S-S. THAT'S WHAT YOU CALL A MISS!!!")
+p football_cheerleader.yell_happily("PUMP! PUMP! PUMP IT UP!!!!")
