c458b8b45a62e1544c810a814beb8441a7b8caa1
echo 'hello world <lisathuanguyen@gmail.com>
2016-10-03 07:44:39 -0700

    Saving convert class

--- ruby/take_a_guess/take_a_guess_game.rb
+++ ruby/take_a_guess/take_a_guess_game.rb
@@ -6,4 +6,17 @@ class Take_a_guess
 		@total_guesses_available = 0
 		@guess_array = []
 	end
+
+	def convert(word_or_phrase)
+		@hidden_phrase = []
+		word_or_phrase.downcase.split('').each do |character|
+			if character == ' '
+				@hidden_phrase << ' '
+			else
+				@hidden_phrase << '_'
+			end
+		end
+		@hidden_phrase = @hidden_phrase.join
+		return @hidden_phrase
+	end
 end
