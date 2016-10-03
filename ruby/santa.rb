7bfad712503f85e6b20edd71a462665ddeb3e973
echo 'hello world <lisathuanguyen@gmail.com>
2016-10-02 22:07:00 -0700

    Saving speak, eat_milk_and_cookies, and initialize methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,17 @@
 class Santa
+	def speak
+		puts "Ho, Ho, Ho! Haaaappy Holidays!"
+	end
 
+	def eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type} cookie!"
+	end
+
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
 end
