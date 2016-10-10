37f6d1dfe307fa506a90ca2504ee0efab4635dee
echo 'hello world <lisathuanguyen@gmail.com>
2016-10-10 08:43:12 -0700

    Saving new file EXPLORE.JS and the pseudocode

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// Pseudocode a function that takes a string as a parameter and reverses the string.
+
+// 1. The string is passed into the function
+// 2. Each character is iterated though the string and the character is given an index value of the length of the total string, 
+// minus the index value
+// 3. The string is then reassembled based off of the index value for each character
