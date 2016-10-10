c3f18d73b070d368bb3179e261a54b7d1d8534c7
echo 'hello world <lisathuanguyen@gmail.com>
2016-10-10 08:44:00 -0700

    Saving code for release 4

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,21 @@
 // 2. Each character is iterated though the string and the character is given an index value of the length of the total string, 
 // minus the index value
 // 3. The string is then reassembled based off of the index value for each character
+
+function reverse_string(str) {
+
+	reversed_string = ""
+
+	for (var i = 1; i <= str.length; i++) {
+		reversed_string += (str[str.length - i])
+	}
+	return reversed_string;
+}
+
+var stored_string = reverse_string("I love JavaScript! (seriously!!!)");
+
+if (stored_string.length > 3) {
+	console.log(stored_string);
+} else {
+	console.log("String is too short to print");
+}
