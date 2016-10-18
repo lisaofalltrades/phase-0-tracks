98a2c6ee26709e1f4204500f3d705e1304679289
lisaofalltrades <lisathuanguyen@gmail.com>
2016-10-17 17:14:42 -0700

    Saving pseudocode for method to create a list

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,31 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # set an empty array called grocery items
+  # split string of items 
+  # place separated items into a hash
+  # set default quantity
+  # print the list to the console [use print method]
+# output: hash
+
+
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps:
+# output:
+
+# Method to remove an item from the list
+# input:
+# steps:
+# output:
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
