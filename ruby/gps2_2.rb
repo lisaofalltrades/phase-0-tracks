12c9ab9793f6bd94220d95679b7aa8597014fa1f
lisaofalltrades <lisathuanguyen@gmail.com>
2016-10-17 19:37:50 -0700

    Saving remove_item and update_item method

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,54 +1,64 @@
-# Method to print a list and make it look pretty
-# input: iterate through each key/value pair
-# steps: print on screen
-# output: list
+=begin
+##### PRINT_LIST METHOD #####
+input: iterate through each key/value pair
+steps: print on screen
+output: list of item and quantity
+
+##### LIST METHOD #####
+input: string of items separated by spaces (example: "carrots apples cereal pizza")
+steps: 
+  set an empty array called grocery items
+  split string of items 
+  place separated items into a hash
+  set default quantity
+  print the list to the console [use print method]
+output: hash
+
+##### ADD_ITEM METHOD #####
+input: item to be added and optional quantity
+output: updated list of item and quantity
+
+##### REMOVE_ITEM METHOD #####
+input: item to be removed
+output: updated list of item and quantity
+
+##### UPDATE_ITEM METHOD #####
+input: item name and quantity
+output: updated list of item and quantity
+
+=end
 
 def print_list(list)
-	list.each do |item, quantity|
-	puts "#{item}: #{quantity}"
-	end
+	puts "Here is your updated list:"
+	list.each { |item, quantity| puts "#{item}: #{quantity}" }
 end
 
-# Method to create a list
-# input: string of items separated by spaces (example: "carrots apples cereal pizza")
-# steps: 
-  # set an empty array called grocery items
-  # split string of items 
-  # place separated items into a hash
-  # set default quantity
-  # print the list to the console [use print method]
-# output: hash
-def list(string)
+def create_list(string)
 	grocery_items = string.split(" ")
-	hash = Hash.new
-	grocery_items.each do |item|
-		hash[item] = 1
-	end
-	print_list(hash)
+	list = Hash.new
+	grocery_items.each { |item| list[item] = 1 }
+	print_list(list)
 end
 
-grocery_items = list("carrots apples cereal pizza")
-
-# Method to add an item to a list
-# input: item name and optional quantity
-# steps:
-# output:
+grocery_items = create_list("carrots apples cereal pizza")
 
 def add_item(list,new_item,quantity=1)
 	list[new_item] = quantity
 end
 
 add_item(grocery_items,"potato", 5)
-
 print_list(grocery_items)
 
-# Method to remove an item from the list
-# input:
-# steps:
-# output:
+def remove_item(list, item)
+	list.delete(item)
+end
 
-# Method to update the quantity of an item
-# input:
-# steps:
-# output:
+remove_item(grocery_items, "potato")
+print_list(grocery_items)
 
+def update_item(list, item, quantity)
+	list[item] = quantity
+end
+
+update_item(grocery_items, "apples", 5)
+print_list(grocery_items)
