9d06b0c9437026bb2469720eb1364b9e376982ef
lisaofalltrades <lisathuanguyen@gmail.com>
2016-11-11 15:40:31 -0800

    Saving assignment 9.4

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,47 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a GET route that displays
+# an address
+get '/contact' do
+  "<footer>6620 Chase way<br>San Diego CA<br>92130</footer>"
+end
+
+# write a GET route that takes
+# a persons name as a query parameter
+# and say "Good job, [person's name]!"
+get '/goodjob/' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+# A route that uses route parameters 
+# to add two numbers and respond 
+# with the result.
+get '/addnumbers/' do
+  number1 = params[:x]
+  number2 = params[:y]
+  sum = (number1.to_i + number2.to_i).to_s
+end
+
+# Make a route that allows the user to
+# search the database in some way -- 
+# maybe for students who have a certain
+# first name, or some other attribute
+get '/students/' do
+  name = params[:name]
+  students = db.execute("SELECT * FROM students")
+  like_named_students = ""
+  students.each do |student|
+    if student['name'].include?(name)
+      like_named_students << "ID: #{student['id']}<br>"
+      like_named_students << "Name: #{student['name']}<br>"
+    end
+  end
+  like_named_students
+end
